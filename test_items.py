d493c58d1a1b59f8d777fd2b1c6cf85f1c2164d0
Dmitry Trubin <boysss-92@mail.ru>
2023-06-16 10:02:30 +0300

    Second commit

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_diffirent_languages(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(30)
     browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    
